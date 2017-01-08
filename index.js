e3ba063fb16658ac4732792821808f884456ae8e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-01-08 12:35:22 -0800

    established a hierarchy for the application routers

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const checkAuthToken = require('./checkAuthToken');
+const lightsRouter = require('./lights');
 
 const app = express();
+const apiRouter = express.Router(); // eslint-disable-line new-cap
 
 const appPort = process.env.PORT || process.argv[2] || 8080;
 
@@ -19,5 +22,9 @@ app.get('/', (req, res) => {
         bridge: process.env.HUE_BRIDGE_ADDRESS
     });
 });
+
+apiRouter.use('/lights', lightsRouter);
+
+app.use('/api', apiRouter);
 
 app.listen(appPort, () => console.log(`Hue remote now listening at ${appPort}...`));
