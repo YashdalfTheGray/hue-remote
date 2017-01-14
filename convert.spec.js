897fd784912503615517708c3aee4fd2a17ecdb3
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-01-14 13:40:33 -0800

    reformated the tests to have better output

--- convert.spec.js
+++ convert.spec.js
@@ -1,46 +1,44 @@
 const test = require('tape');
 const convert = require('./convert');
 
-test('convertColor', t => {
-    t.test('handles an rgb string', t => {
-        const result = convert.rgbToHue('#deadaf');
+test('convertColor handles an rgb string', t => {
+    const result = convert.rgbToHue('#deadaf');
 
-        t.equal(result.status, 'ok');
-        t.end();
-    });
+    t.equal(result.status, 'ok');
+    t.end();
+});
 
-    t.test('handles an rgb value array', t => {
-        const result = convert.rgbToHue([34, 52, 240]);
+test('convertColor handles an rgb value array', t => {
+    const result = convert.rgbToHue([34, 52, 240]);
 
-        t.equal(result.status, 'ok');
-        t.end();
-    });
+    t.equal(result.status, 'ok');
+    t.end();
+});
 
-    t.test('handles a malformed rgb string', t => {
-        const result = convert.rgbToHue('#dead');
+test('convertColor handles a malformed rgb string', t => {
+    const result = convert.rgbToHue('#dead');
 
-        t.equal(result.status, 'error');
-        t.end();
-    });
+    t.equal(result.status, 'error');
+    t.end();
+});
 
-    t.test('handles a malformed rgb array', t => {
-        const result = convert.rgbToHue([124, 120]);
+test('convertColor handles a malformed rgb array', t => {
+    const result = convert.rgbToHue([124, 120]);
 
-        t.equal(result.status, 'error');
-        t.end();
-    });
+    t.equal(result.status, 'error');
+    t.end();
+});
 
-    t.test('handles na rgb array with illegal values', t => {
-        const result = convert.rgbToHue([124, 120, 300]);
+test('convertColor handles na rgb array with illegal values', t => {
+    const result = convert.rgbToHue([124, 120, 300]);
 
-        t.equal(result.status, 'error');
-        t.end();
-    });
+    t.equal(result.status, 'error');
+    t.end();
+});
 
-    t.test('handles no input', t => {
-        const result = convert.rgbToHue();
+test('convertColor handles no input', t => {
+    const result = convert.rgbToHue();
 
-        t.equal(result.status, 'error');
-        t.end();
-    });
+    t.equal(result.status, 'error');
+    t.end();
 });
