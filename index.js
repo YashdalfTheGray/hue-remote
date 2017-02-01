d29d948bcb635e96ec9bd6818151c1f3c3238ac8
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-01-31 20:47:14 -0800

    letsencrypt-verify mode doesn't load up any of the server

--- index.js
+++ index.js
@@ -13,35 +13,6 @@ const helmet = require('helmet');
 const checkAuthToken = require('./util/checkAuthToken');
 const lightsRouter = require('./endpoints/lights');
 
-const app = express();
-const apiRouter = express.Router(); // eslint-disable-line new-cap
-
-const appPort = process.env.PORT || process.argv[2] || 8080;
-const cert = {
-    key: fs.readFileSync('./sslcert/key.pem'),
-    cert: fs.readFileSync('./sslcert/cert.pem')
-};
-
-app.use(bodyParser.json());
-app.use(morgan('common'));
-app.use(helmet());
-
-app.get('/', (req, res) => {
-    res.json({
-        status: 'ok',
-        bridgeFound: !!process.env.HUE_BRIDGE_ADDRESS,
-        bridgeUserFound: !!process.env.HUE_BRIDGE_USERNAME,
-        apiTokenFound: !!process.env.HUE_REMOTE_TOKEN
-    });
-});
-
-apiRouter.use(checkAuthToken);
-apiRouter.use('/lights', lightsRouter);
-
-app.use('/api', apiRouter);
-
-https.createServer(cert, app).listen(appPort, () => console.log(`Hue remote now listening at ${chalk.green(appPort)}...`));
-
 if (process.argv.filter(a => a === '--letsencrypt-verify').length > 0) {
     const httpApp = express();
 
@@ -55,4 +26,34 @@ if (process.argv.filter(a => a === '--letsencrypt-verify').length > 0) {
 
     httpApp.use(express.static('static'));
     httpApp.listen(8080, () => console.log(`Let's Encrypt verify server running at ${chalk.green('8080')}...`));
+}
+else {
+    const app = express();
+    const apiRouter = express.Router(); // eslint-disable-line new-cap
+
+    const appPort = process.env.PORT || process.argv[2] || 8080;
+    const cert = {
+        key: fs.readFileSync('./sslcert/key.pem'),
+        cert: fs.readFileSync('./sslcert/cert.pem')
+    };
+
+    app.use(bodyParser.json());
+    app.use(morgan('common'));
+    app.use(helmet());
+
+    app.get('/', (req, res) => {
+        res.json({
+            status: 'ok',
+            bridgeFound: !!process.env.HUE_BRIDGE_ADDRESS,
+            bridgeUserFound: !!process.env.HUE_BRIDGE_USERNAME,
+            apiTokenFound: !!process.env.HUE_REMOTE_TOKEN
+        });
+    });
+
+    apiRouter.use(checkAuthToken);
+    apiRouter.use('/lights', lightsRouter);
+
+    app.use('/api', apiRouter);
+
+    https.createServer(cert, app).listen(appPort, () => console.log(`Hue remote now listening at ${chalk.green(appPort)}...`));
 }
