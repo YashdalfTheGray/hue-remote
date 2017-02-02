6de643e435e57b7ccd17057edc7c99bb20f9c86b
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-02-01 21:08:40 -0800

    started translating the groups hue local api

---
+++ endpoints/groups.js
@@ -0,0 +1,54 @@
+const Router = require('express').Router;
+const request = require('request-promise');
+
+const checkAuthToken = require('../util/checkAuthToken');
+
+const groupsRouter = Router(); // eslint-disable-line new-cap
+
+groupsRouter.get('/', checkAuthToken, (req, res) => {
+    const hueUser = process.env.HUE_BRIDGE_USERNAME;
+    const hueBridge = process.env.HUE_BRIDGE_ADDRESS;
+
+    request({
+        method: 'GET',
+        url: 'http://' + hueBridge + '/api/' + hueUser + '/groups',
+        json: true
+    }).then(result => {
+        res.json(Object.keys(result).reduce((acc, k) => {
+            acc.push({
+                id: k,
+                name: result[k].name,
+                lightIds: result[k].lights,
+                state: result[k].state
+            });
+            return acc;
+        }, []));
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+groupsRouter.get('/:id', checkAuthToken, (req, res) => {
+    const hueUser = process.env.HUE_BRIDGE_USERNAME;
+    const hueBridge = process.env.HUE_BRIDGE_ADDRESS;
+
+    request({
+        method: 'GET',
+        url: 'http://' + hueBridge + '/api/' + hueUser + '/groups/' + req.params.id,
+        json: true
+    }).then(result => {
+        res.json({
+            id: req.params.id,
+            name: result.name,
+            lightIds: result.lightIds,
+            state: result.state,
+            action: result.action
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = groupsRouter;
