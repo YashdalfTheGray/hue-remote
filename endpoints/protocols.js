1fa6388c8772f9de1b73dff450c5a493148a0d21
YashdalfTheGray <yash.kulshrestha@gmail.com>
2017-08-03 21:28:33 -0700

    added basic crud operations to the protocols stuff

--- endpoints/protocols.js
+++ endpoints/protocols.js
@@ -14,7 +14,49 @@ const getOneProtocol = async (req, res) => {
     const client = res.locals.redis;
 
     try {
-        const value = await client.hgetallAsync(req.params.id);
+        const value = await client.hgetallAsync(req.params.name);
+
+        if (!value) {
+            res.sendStatus(404);
+        }
+        else {
+            res.json(value);
+        }
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+};
+
+const createProtocol = async (req, res) => {
+    const client = res.locals.redis;
+
+    try {
+        const value = await client.hmsetAsync(req.body.name, req.body.details);
+        res.status(201).json(value);
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+};
+
+const deleteProtocol = async (req, res) => {
+    const client = res.locals.redis;
+
+    try {
+        const value = await client.delAsync(req.params.name);
+        res.json(value);
+    }
+    catch (e) {
+        res.status(500).json(e);
+    }
+};
+
+const updateProtocol = async (req, res) => {
+    const client = res.locals.redis;
+
+    try {
+        const value = await client.hmsetAsync(req.params.name, req.body);
         res.json(value);
     }
     catch (e) {
@@ -24,5 +66,8 @@ const getOneProtocol = async (req, res) => {
 
 module.exports = {
     getProtocols,
-    getOneProtocol
+    getOneProtocol,
+    createProtocol,
+    deleteProtocol,
+    updateProtocol
 };
