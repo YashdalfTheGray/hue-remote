2b97919de4471b2e9b3c7bbdb5f73c21b4958e23
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-03-24 08:50:45 -0700

    added a node-fetch version of post group id action

--- endpoints/groups.js
+++ endpoints/groups.js
@@ -141,10 +141,45 @@ const postGroupIdAction = (req, res) => {
   }
 };
 
+const postGroupIdActionAsync = async (req, res) => {
+  const hueUser = process.env.HUE_BRIDGE_USERNAME;
+  const hueBridge = process.env.HUE_BRIDGE_ADDRESS;
+  const validKeys = ['on', 'color', 'colorTemp', 'colorloop'];
+
+  const validRequest = Object.keys(req.body).reduce(
+    (acc, k) => acc || validKeys.indexOf(k) !== -1,
+    false
+  );
+
+  if (validRequest) {
+    try {
+      const response = await fetch(
+        `http://${hueBridge}/api/${hueUser}/groups/${req.params.id}/action`,
+        {
+          method: 'PUT',
+          body: mapToActionObject(req.body)
+        }
+      );
+      const json = await response.json();
+
+      res.json(json);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(400).json({
+      status: 400,
+      message: 'Malformed request body'
+    });
+  }
+};
+
 module.exports = {
   getGroupsRoot,
   getGroupsRootAsync,
   getGroupsId,
   getGroupsIdAsync,
-  postGroupIdAction
+  postGroupIdAction,
+  postGroupIdActionAsync
 };
