d1f3d7e49ae83db1afef7a960e3d47940ed696cd
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-03-24 08:45:41 -0700

    added a node-fetch based handler for groups/id

--- endpoints/groups.js
+++ endpoints/groups.js
@@ -82,6 +82,33 @@ const getGroupsId = (req, res) => {
     });
 };
 
+const getGroupsIdAsync = async (req, res) => {
+  const hueUser = process.env.HUE_BRIDGE_USERNAME;
+  const hueBridge = process.env.HUE_BRIDGE_ADDRESS;
+
+  try {
+    const response = await fetch(
+      `http://${hueBridge}/api/${hueUser}/groups/${req.params.id}`,
+      {
+        method: 'GET'
+      }
+    );
+
+    const json = await response.json();
+
+    res.json({
+      id: req.params.id,
+      name: json.name,
+      lightIds: json.lights,
+      state: json.state,
+      action: mapFromActionObject(json.action)
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
 const postGroupIdAction = (req, res) => {
   const hueUser = process.env.HUE_BRIDGE_USERNAME;
   const hueBridge = process.env.HUE_BRIDGE_ADDRESS;
@@ -118,5 +145,6 @@ module.exports = {
   getGroupsRoot,
   getGroupsRootAsync,
   getGroupsId,
+  getGroupsIdAsync,
   postGroupIdAction
 };
