81090e1be768e65388ece8a01cd02671e57decbc
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-10-13 21:42:18 -0700

    added another transport to the logger class

--- util/Logger.js
+++ util/Logger.js
@@ -10,11 +10,17 @@ class Logger {
   constructor() {
     this.logger = winston.createLogger({
       level: 'info',
-      format: winston.format.json(),
       defaultMeta: { application: 'hue-remote' },
       transports: [
         new winston.transports.File({
-          filename: path.resolve(getLogsPath(), 'application.log')
+          filename: path.resolve(getLogsPath(), 'application.log'),
+          format: winston.format.json()
+        }),
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple()
+          )
         })
       ]
     });
