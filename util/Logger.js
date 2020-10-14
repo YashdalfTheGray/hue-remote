6a547136e696aac835aedd6d28c3352d306ae693
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-10-14 06:55:48 -0700

    node's internal package cache should make our logger a singleton already

--- util/Logger.js
+++ util/Logger.js
@@ -1,45 +1,30 @@
-/* eslint-disable max-classes-per-file */
-
 const path = require('path');
 
 const winston = require('winston');
 
 const { getLogsPath } = require('./logs');
 
-class Logger {
-  constructor() {
-    this.logger = winston.createLogger({
-      level: 'info',
-      defaultMeta: { application: 'hue-remote' },
-      transports: [
-        new winston.transports.File({
-          filename: path.resolve(getLogsPath(), 'application.log'),
-          format: winston.format.json()
-        }),
-        new winston.transports.Console({
-          format: winston.format.combine(
-            winston.format.colorize(),
-            winston.format.simple()
-          )
-        })
-      ],
-      exceptionHandlers: [
-        new winston.transports.File({
-          filename: path.resolve(getLogsPath(), 'uncaught-exceptions.log'),
-          format: winston.format.json()
-        })
-      ]
-    });
-  }
-}
+const logger = winston.createLogger({
+  level: 'info',
+  defaultMeta: { application: 'hue-remote' },
+  transports: [
+    new winston.transports.File({
+      filename: path.resolve(getLogsPath(), 'application.log'),
+      format: winston.format.json()
+    }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      )
+    })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({
+      filename: path.resolve(getLogsPath(), 'uncaught-exceptions.log'),
+      format: winston.format.json()
+    })
+  ]
+});
 
-class Singleton {
-  getInstance() {
-    if (!Singleton.instance) {
-      Singleton.instance = new Logger();
-    }
-    return Singleton.instance;
-  }
-}
-
-module.exports = Singleton;
+module.exports = logger;
