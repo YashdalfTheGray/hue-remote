4d1d70999955ac252707e1054a43e55efa9609d7
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-01-16 14:12:45 -0500

    we no longer need to promisify the redis stuff

--- util/redis.js
+++ util/redis.js
@@ -1,11 +1,7 @@
-const { promisifyAll } = require('bluebird');
 const redis = require('redis');
 
-promisifyAll(redis.RedisClient.prototype);
-promisifyAll(redis.Multi.prototype);
-
 module.exports = {
-  setupRedis: url => redis.createClient(url),
+  setupRedis: url => redis.createClient({ url }),
   injectRedis: rc => (req, res, next) => {
     res.locals.redis = rc;
     next();
