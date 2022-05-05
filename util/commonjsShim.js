55131bd479b949bcafa733c799f40633e0fe1464
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2022-05-05 01:51:39 -0700

    since __dirname is not defined, created a shim to backfill

---
+++ util/commonjsShim.js
@@ -0,0 +1,7 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// eslint-disable-next-line no-underscore-dangle
+export const __filename = fileURLToPath(import.meta.url);
+// eslint-disable-next-line no-underscore-dangle
+export const __dirname = path.dirname(__filename);
--- util/logs.js
+++ util/logs.js
@@ -1,5 +1,7 @@
 import path from 'path';
 
+import { __dirname } from './commonjsShim.js';
+
 // eslint-disable-next-line import/prefer-default-export
 export const getLogsPath = () => {
   const LogsPathFromEnvOrDefault =
